808cd1f9378852ad11b03641f91c61b93dc224f7
jesusnoseq <jesusnoseq@hotmail.com>
2025-04-27 10:30:34 +0200

    chore: remove unused files

--- back/build.sh
+++
@@ -1,2 +0,0 @@
-#GOOS=js GOARCH=wasm go build -o main.wasm
-GOOS=linux GOARCH=amd64 go build -o back
--- back/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bootstrap
-zip function.zip bootstrap
-sam deploy --template-file template.yaml --stack-name AoE4Friends --capabilities CAPABILITY_IAM
